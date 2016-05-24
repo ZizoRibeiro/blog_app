8961d1be48aef306d38dbf807050dce21d6c7e83
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-05-24 13:20:18 -0700

    Modified guardfile

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-	root to: 'articles#index'
-	resources :articles
+  root to: 'articles#index'
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
